004c5fa9317ca8f09c34b6b08de2792ddf894934
jacob <jacob.henningsson95@gmail.com>
2023-03-23 14:45:33 +0100

    scalability_experiments.py: - Added type of scalability test to the directory name. - Increased strong scaling sample fraction to 1.0 from 0.1 to have more data. - Divided weak scaling sample fractions into 1/6 increments. - Added Timing for whole loop. - Added count after command to RDD from the pipline which will trigger spark to calculate all RDDs. - Added spark session close between loop runs to enable core amount updates. Except for the last run where the RDD is needed to print output.
    sub_overlap_pipeline.py:
    - Specified 2GB RAM per executor in the spark configuration.
    - Removed the limit on amount of subreddits and renamed filter_top_subreddits to count_subreddits.
    
    sub_overlap_run.py:
    - Removed all timing between steps since this was miss leading due to no calculations being performed, spark only creates transforms.
    - Changed the dataset variable.

--- spark/sub_overlap_run.py
+++ spark/sub_overlap_run.py
@@ -4,64 +4,37 @@ import time
 from sub_overlap_pipeline import *
 
 
-def run_pipeline(max_cores, subs_to_incl, comment_threshold, sample_fraction):
+def run_pipeline(max_cores, comment_threshold, sample_fraction):
     """Run all the steps in the pipeline in sequence"""
 
     # Spark cluster and HDFS settings
     master_address = "spark://spark-master:7077"
     app_name = "group_12_app"
-    hdfs_path = "hdfs://spark-master:9000/user/root/RC_2009-04.json"
-
-    # Dictionary for saving runtime
-    times = {}
+    hdfs_path = "hdfs://spark-master:9000/user/root/RC_2009-12.json"
 
     # Step 1: Start spark session and load data
-    start_time = time.perf_counter()
     spark_session = start_session(master_address, app_name, max_cores)
     df_raw = load_data(hdfs_path, spark_session)
-    end_time = time.perf_counter()
-    times["start_session_and_load_data"] = round(end_time - start_time, 2)
 
     # Step 2: Filter out columns that will not be used
-    start_time = time.perf_counter()
     df_reddit = filter_columns_and_sample(df_raw, sample_fraction)
-    end_time = time.perf_counter()
-    times["filter_columns"] = round(end_time - start_time, 2)
 
     # Step 3: Count the biggest subreddits and filter dataframe for those
-    start_time = time.perf_counter()
-    df_subred_count, df_sub_filtered = filter_top_subreddits(df_reddit, subs_to_incl)
-    end_time = time.perf_counter()
-    times["filter_top_subreddits"] = round(end_time - start_time, 2)
+    df_subred_count, df_sub_filtered = count_subreddits(df_reddit)
 
     # Step 4: Get list with users with significant amount of posts and filter dataframe
-    start_time = time.perf_counter()
     df_user_filtered = filter_top_users(df_sub_filtered, comment_threshold)
-    end_time = time.perf_counter()
-    times["filter_top_users"] = round(end_time - start_time, 2)
 
     # Step 5: Generate all subreddit combinations for each user
-    start_time = time.perf_counter()
     df_user_subs = create_user_subreddit_tuples(df_user_filtered)
-    end_time = time.perf_counter()
-    times["create_user_subreddit_tuples"] = round(end_time - start_time, 2)
 
     # Step 6: Count how many times each subreddit combination appears
-    start_time = time.perf_counter()
     df_tuple_counts = count_tuples(df_user_subs)
-    end_time = time.perf_counter()
-    times["count_tuples"] = round(end_time - start_time, 2)
 
     # Step 7: Remove duplicates from data
-    start_time = time.perf_counter()
     df_no_dupes = remove_duplicates(df_tuple_counts)
-    end_time = time.perf_counter()
-    times["remove_duplicates"] = round(end_time - start_time, 2)
 
     # Step 8: Join dataframes with counts together
-    start_time = time.perf_counter()
     df_result = join_count_data(df_subred_count, df_no_dupes)
-    end_time = time.perf_counter()
-    times["join_count_data"] = round(end_time - start_time, 2)
 
-    return df_result, times, spark_session
+    return df_result, spark_session
